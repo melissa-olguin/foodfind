a8a795de92ff5afa6a4d27e3cb795370b6b76ef5
Melissa Olguin <57164061+melissa-olguin@users.noreply.github.com>
2020-02-10 13:07:06 -0600

    first polished app

---
+++ src/components/recipe/recipe.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./recipe.css";
+
+export const Recipe = ({ title, calories, image, ingredients, url }) => (
+  <a href={url}>
+    <div className="recipe">
+      <h1 className="recipe_title">{title}</h1>
+      <div className="image_div">
+        <img className="image" src={image} alt="" />
+      </div>
+      <p className="calories">Calories: {parseInt(calories)} Kcal</p>
+      <h2>Ingredients:</h2>
+      <ul className="ingredients">
+        {ingredients.map((ingredient, index) => (
+          <li className="ingredient" key={index}>
+            <i className="fas fa-caret-right" /> {ingredient.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </a>
+);
